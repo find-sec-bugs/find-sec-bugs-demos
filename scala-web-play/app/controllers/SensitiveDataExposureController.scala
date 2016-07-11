31314676115ca1f202aada2759bfe228ad5d89d7
MaxNad <maxime.nadeau@outlook.com>
2016-07-11 13:16:40 -0400

    Sensitive data exposure (CWE 200) - vulnerable code detector for Scala

--- scala-web-play/app/controllers/SensitiveDataExposureController.scala
+++ scala-web-play/app/controllers/SensitiveDataExposureController.scala
@@ -28,12 +28,19 @@ class SensitiveDataExposureController @Inject() (configuration: play.api.Configu
 
   def vulnerable2(value:String) = Action {
     val systemProperty = System.getProperty(value)
+    val systemProperty2 = System.getProperty(value, "val")
 
     NotFound("Result:\n"+systemProperty)
     BadRequest("Result:\n"+systemProperty)
     InternalServerError("Result:\n"+systemProperty)
     Status(418)("Result:\n"+systemProperty)
     Ok("Result:\n"+systemProperty)
+
+    NotFound("Result:\n"+systemProperty2)
+    BadRequest("Result:\n"+systemProperty2)
+    InternalServerError("Result:\n"+systemProperty2)
+    Status(418)("Result:\n"+systemProperty2)
+    Ok("Result:\n"+systemProperty2)
   }
 
   def vulnerable3(value:String) = Action {
@@ -46,7 +53,7 @@ class SensitiveDataExposureController @Inject() (configuration: play.api.Configu
     Ok("Result:\n"+systemEnvValue)
   }
 
-  def safe1() = Action {
+  def safePlayConfig() = Action {
     // Play 2.5
     val configElementLatest = configuration.underlying.getString("application.test")
 
@@ -66,8 +73,16 @@ class SensitiveDataExposureController @Inject() (configuration: play.api.Configu
     Ok("Result:\n"+configElement2)
   }
 
-  def safe2() = Action {
-    val systemEnvValue = System.getenv("MaxPermSize")
+  def safeEnvProperty() = Action {
+    val systemProperty = System.getProperty("Xpermsize")
+
+    val systemEnvValue = System.getenv("path")
+
+    NotFound("Result:\n"+systemProperty)
+    BadRequest("Result:\n"+systemProperty)
+    InternalServerError("Result:\n"+systemProperty)
+    Status(418)("Result:\n"+systemProperty)
+    Ok("Result:\n"+systemProperty)
 
     NotFound("Result:\n"+systemEnvValue)
     BadRequest("Result:\n"+systemEnvValue)
@@ -76,13 +91,41 @@ class SensitiveDataExposureController @Inject() (configuration: play.api.Configu
     Ok("Result:\n"+systemEnvValue)
   }
 
-  def safe3() = Action {
-    val systemEnvValue = System.getenv("path")
+  def safeUntainted(value:String) = Action {
+    // Play 2.5
+    val configElementLatest = configuration.underlying.getString(value)
 
-    NotFound("Result:\n"+systemEnvValue)
-    BadRequest("Result:\n"+systemEnvValue)
-    InternalServerError("Result:\n"+systemEnvValue)
-    Status(418)("Result:\n"+systemEnvValue)
-    Ok("Result:\n"+systemEnvValue)
+    // Play 2.0
+    val configElement2 = Play.current.configuration.getString(value)
+
+    val systemProperty = System.getProperty(value)
+
+    val systemEnvValue = System.getenv(value)
+
+    NotFound("Result:\n"+"Done.")
+    BadRequest("Result:\n"+"Done.")
+    InternalServerError("Result:\n"+"Done.")
+    Status(418)("Result:\n"+"Done.")
+    Ok("Result:\n"+"Done.")
+
+    NotFound("Result:\n"+value)
+    BadRequest("Result:\n"+value)
+    InternalServerError("Result:\n"+value)
+    Status(418)("Result:\n"+value)
+    Ok("Result:\n"+value)
+  }
+
+  def safeNoSensitiveData(value:String) = Action {
+    NotFound("Result:\n"+"Done.")
+    BadRequest("Result:\n"+"Done.")
+    InternalServerError("Result:\n"+"Done.")
+    Status(418)("Result:\n"+"Done.")
+    Ok("Result:\n"+"Done.")
+
+    NotFound("Result:\n"+value)
+    BadRequest("Result:\n"+value)
+    InternalServerError("Result:\n"+value)
+    Status(418)("Result:\n"+value)
+    Ok("Result:\n"+value)
   }
 }
