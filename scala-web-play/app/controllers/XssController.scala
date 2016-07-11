0a6a6ede1a81f18559e7e1080597763d27f1ced4
MaxNad <maxime.nadeau@outlook.com>
2016-07-11 18:29:40 -0400

    XSS (CWE 79) - Added some more Scala Play vulnerable code samples

--- scala-web-play/app/controllers/XssController.scala
+++ scala-web-play/app/controllers/XssController.scala
@@ -1,14 +1,30 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.twirl.api.Html;
 
 class XssController extends Controller {
 
-  def xssTest1(value:String) = Action {
-    Ok("Hello "+value+" !")
+  def vulnerable1(value:String) = Action {
+    Ok("Hello "+value+" !").as("text/html")
   }
 
-  def xssTest2(value:String) = Action {
-    Ok("Hello "+value+" !").as("text/html")
+  def vulnerable2(value:String, contentType:String) = Action {
+    Ok("Hello "+value+" !").as(contentType)
+  }
+
+  def vulnerable3(value:String) = Action {
+    Ok(views.html.xssHtml.render(Html.apply("Hello "+value+" !")))
+  }
+
+  def potentiallyVulnerable(value:String) = Action {
+    Ok(views.html.xssString.render("Hello !"))
+  }
+
+  def variousSafe(value:String) = Action {
+    Ok("Hello "+value+" !")
+    Ok(s"Hello $value !").as("text/json")
+    Ok("<b>Hello !</b>").as("text/html")
+    Ok(views.html.xssHtml.render(Html.apply("<b>Hello !</b>")))
   }
 }
