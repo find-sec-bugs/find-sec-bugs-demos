c1a5a875db89742a818644937ed469781a714ad9
MaxNad <maxime.nadeau@outlook.com>
2016-09-15 09:08:51 -0400

    Path Traversal (CWE 22) - Basic Path Traversal vulnerable code samples

---
+++ scala-web-play/app/controllers/PathTraversalController.scala
@@ -0,0 +1,47 @@
+package controllers
+
+import java.io.File
+import java.net.URI
+
+import play.api.mvc.{Action, Controller}
+
+import scala.io.Source
+
+class PathTraversalController extends Controller {
+
+  def vulnerableIn(value:String) = Action {
+    Source.fromFile(new File(value))
+    Source.fromFile(new File(value), 100)
+    Source.fromFile(new File(value), "UTF-8")
+    Source.fromFile(value).getLines()
+    Source.fromFile(value, "UTF-8")
+    Source.fromFile(new URI(value))
+    val result = Source.fromFile(new URI(value), "UTF-8")
+
+    Ok("Result:\n"+result)
+  }
+
+  def vulnerableOut(value:String, value2:String) = Action {
+    reflect.io.File(value)
+
+    Ok("Result:\n"+"Done.")
+  }
+
+  def safeIn(value:String) = Action {
+    Source.fromFile(new File("Safe"))
+    Source.fromFile(new File("Safe"), 100)
+    Source.fromFile(new File("Safe"), "UTF-8")
+    Source.fromFile("Safe").getLines()
+    Source.fromFile("Safe", "UTF-8")
+    Source.fromFile(new URI("Safe"))
+    val result = Source.fromFile(new URI("Safe"), "UTF-8")
+
+    Ok("Result:\n"+result)
+  }
+
+  def safeOut(value:String) = Action {
+    reflect.io.File("Safe")
+
+    Ok("Result:\n"+"Done.")
+  }
+}
