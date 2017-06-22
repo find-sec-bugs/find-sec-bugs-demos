d7842ce0bbab20c4ed44bbc8b52dfe1c6cc8becf
MaxNad <maxime.nadeau@outlook.com>
2017-06-22 12:11:13 -0400

    Changed librairies version for Scala 2.11.8 with Play 2.5.8 to support newer methods

--- scala-web-play/app/controllers/SSRFController.scala
+++ scala-web-play/app/controllers/SSRFController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api.libs.ws._
 import play.api.mvc.{Action, Controller}
+import javax.inject._
 
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits._
@@ -12,9 +13,21 @@ import play.api.libs.concurrent.Execution.Implicits._
   * See CWE-918 :
   *     https://cwe.mitre.org/data/definitions/918.html
   */
-class SSRFController extends Controller {
+class SSRFController @Inject() (wSClient: WSClient)  extends Controller {
 
   def vulnerableGet(value:String) = Action.async {
+
+    // ####################
+    // SBT 2.5.x
+    // ####################
+    // This method could expose internal services like mongodb to an attacker
+    wSClient.url(value).get().map { response =>
+      Ok(response.body)
+    }
+
+    // ####################
+    // SBT 2.4.x
+    // ####################
 
     // This method could expose internal services like mongodb to an attacker
     WS.url(value).get().map { response =>
@@ -24,6 +37,26 @@ class SSRFController extends Controller {
 
   def vulnerablePost(value:String, postValue:String) = Action.async {
 
+    // ####################
+    // SBT 2.5.x
+    // ####################
+    // These method could expose internal services like mongodb to an attacker
+    wSClient.url(value).post(postValue).map { response =>
+      Ok(response.body)
+    }
+
+    wSClient.url(value).post("key=value").map { response =>
+      Ok(response.body)
+    }
+
+    // This call could be used to attack another host from our server
+    wSClient.url("http://www.google.com").post(postValue).map { response =>
+      Ok(response.body)
+    }
+
+    // ####################
+    // SBT 2.4.x
+    // ####################
     // These method could expose internal services like mongodb to an attacker
     WS.url(value).post(postValue).map { response =>
       Ok(response.body)
@@ -39,7 +72,33 @@ class SSRFController extends Controller {
     }
   }
 
+  def derp() = {
+    wSClient.url("http://www.google.com").get()
+
+    wSClient.url("http://www.google.com").get().map { response =>
+      Ok(response.body)
+    }
+
+    WS.url("http://www.google.com").get()
+
+    WS.url("http://www.google.com").get().map { response =>
+      Ok(response.body)
+    }
+  }
+
   def safeGetNotTainted() = Action.async {
+    // ####################
+    // Play v.2.5.x
+    // ####################
+    wSClient.url("http://www.google.com").get()
+
+    wSClient.url("http://www.google.com").get().map { response =>
+      Ok(response.body)
+    }
+
+    // ####################
+    // Play v.2.4.x
+    // ####################
     WS.url("http://www.google.com").get()
 
     WS.url("http://www.google.com").get().map { response =>
@@ -48,6 +107,20 @@ class SSRFController extends Controller {
   }
 
   def safePostNotTainted() = Action.async {
+    // ####################
+    // Play v.2.5.x
+    // ####################
+    wSClient.url("http://www.google.com").post("key=value")
+
+    wSClient.url("http://www.google.com").post("key=value").map { response =>
+      Ok(response.body)
+    }
+
+    // ####################
+    // Play v.2.4.x
+    // ####################
+    WS.url("http://www.google.com").post("key=value")
+
     WS.url("http://www.google.com").post("key=value").map { response =>
       Ok(response.body)
     }
@@ -60,6 +133,18 @@ class SSRFController extends Controller {
       url = "http://mysite.com/transfer?amount=" + amount
     }
 
+    // ####################
+    // Play v.2.5.x
+    // ####################
+    wSClient.url("http://www.google.com").get()
+
+    wSClient.url("http://www.google.com").get().map { response =>
+      Ok(response.body)
+    }
+
+    // ####################
+    // Play v.2.4.x
+    // ####################
     WS.url(url).get()
 
     WS.url(url).get().map { response =>
@@ -74,6 +159,18 @@ class SSRFController extends Controller {
       url = "http://mysite.com/transfer"
     }
 
+    // ####################
+    // Play v.2.5.x
+    // ####################
+    wSClient.url(url).post("amount=" + amount)
+
+    wSClient.url(url).post("amount=" + amount).map { response =>
+      Ok(response.body)
+    }
+
+    // ####################
+    // Play v.2.4.x
+    // ####################
     WS.url(url).post("amount=" + amount)
 
     WS.url(url).post("amount=" + amount).map { response =>
